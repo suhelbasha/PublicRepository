d56eaf2867d9ba67dd04707a38ae8bd43a36836e
Suhel Basha <suhel.basha@techveda.org>
2018-02-06 11:22:57 +0530

    commentaryonmodules Branch created
    commentaryonmodules Branch created
    
    Signed-off-by: suhel basha <suhel.basha@techveda.org>

--- modules/myhello.c
+++ modules/myhello.c
@@ -2,16 +2,15 @@
 
 int init_module (void)
 {
-	printk("module inserted \n");
+	printk("module inserted \n"); 			//printk is used here for printing on the console
 	return 0;
-
 }
 
 void cleanup_module (void)
 {
-	printk("module removed\n");
+	printk("module removed\n");			//printk is used here for printing on the console
 }
 
-MODULE_AUTHOR ("suhel.basha@techveda.org");
-MODULE_DESCRIPTION ("LKD_CW: First kernel module");
-MODULE_LICENSE ("GPL");
+MODULE_AUTHOR ("suhel.basha@techveda.org");		//To tell who is the author of this code
+MODULE_DESCRIPTION ("LKD_CW: First kernel module");	//To tell module description
+MODULE_LICENSE ("GPL");					//To tell what license it belongs
